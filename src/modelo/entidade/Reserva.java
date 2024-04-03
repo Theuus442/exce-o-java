8dd4447c8d45c439381ed8664346f796b028858b
Theuus442 <teuzinggs@gmail.com>
2024-04-02 22:03:21 -0300

    Boa solução

--- src/modelo/entidade/Reserva.java
+++ src/modelo/entidade/Reserva.java
@@ -1,5 +1,7 @@
 package modelo.entidade;
 
+import excecao.DomainException;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -11,9 +13,13 @@ public class Reserva {
 
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-    public Reserva(){}
+    public Reserva() {
+    }
 
     public Reserva(Integer numeroQuarto, Date checkin, Date checkout) {
+        if (!checkout.after(checkin)) {
+            throw new DomainException("A data do Check-out é antes do seu Check-in");
+        }
         this.numeroQuarto = numeroQuarto;
         this.checkin = checkin;
         this.checkout = checkout;
@@ -35,18 +41,26 @@ public class Reserva {
         return checkout;
     }
 
-    public long duracao(){
+    public long duracao() {
         long diff = checkout.getTime() - checkin.getTime();
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public void novaDatas(Date checkin, Date checkout){
+    public void novaDatas(Date checkin, Date checkout) {
+        Date agora = new Date();
+
+        if (checkin.before(agora) || checkout.before(agora)) {
+            throw new DomainException("Erro na sua reserva: A reserva tem que ter datas futuras!");
+        }
+        if (!checkout.after(checkin)) {
+            throw new DomainException("A data do Check-out é antes do seu Check-in");
+        }
         this.checkin = checkin;
         this.checkout = checkout;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return "Número do Quarto "
                 + numeroQuarto
                 + ", check-in: "
